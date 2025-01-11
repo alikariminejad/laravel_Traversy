b1a463e0a001258801a133f8e5bd4bdc0d4a83c2
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-01-11 13:29:14 +0330

    Route Params & Constraints

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,27 @@ Route::get('/jobs', function(){
     return '<h1>Available Jobs</h1>';
 })->name('jobs');
 
-Route::match(['get', 'post'],'/submit', function(){
-    return 'submitted';
-});
+// Route::match(['get', 'post'],'/submit', function(){
+//     return 'submitted';
+// });
 
-Route::get('/test', function(){
-    $url = route('jobs');
-    return "<a href='$url'>Click Here</a>";
-});
+// Route::get('/test', function(){
+//     $url = route('jobs');
+//     return "<a href='$url'>Click Here</a>";
+// });
 
-Route::get('/api/users', function(){
-    return [
-        'name'=>'John Doe',
-        'email'=>'john@doe.com',
-    ];
+// Route::get('/api/users', function(){
+//     return [
+//         'name'=>'John Doe',
+//         'email'=>'john@doe.com',
+//     ];
+// });
+
+
+Route::get('/posts/{id}', function (string $id) {
+    return 'Post ' . $id;
+})->whereNumber('id');
+
+Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
+    return 'Post ' . $id . ' Comment ' . $commentId;
 });
