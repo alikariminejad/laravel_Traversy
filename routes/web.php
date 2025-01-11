fe01074456be49be07c6127138ea541c1d8801d4
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-01-11 12:57:38 +0330

    Intro to Routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/jobs', function(){
+    return '<h1>Available Jobs</h1>';
+})->name('jobs');
+
+Route::match(['get', 'post'],'/submit', function(){
+    return 'submitted';
+});
+
+Route::get('/test', function(){
+    $url = route('jobs');
+    return "<a href='$url'>Click Here</a>";
+});
+
+Route::get('/api/users', function(){
+    return [
+        'name'=>'John Doe',
+        'email'=>'john@doe.com',
+    ];
+});
