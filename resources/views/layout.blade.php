d24a77c4b62192195ee48f20664a6a5645a134e3
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-01-15 00:05:46 +0330

    Hero Component

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,12 +6,18 @@
 {{--    <link rel="stylesheet" href="./resources/css/output.css">--}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('./resources/css/output.css')
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
 </head>
 <body class="border-gray-100">
 <x-header/>
+@if(request()->is('/'))
+<x-hero />
+@endif
 <main class="container mx-auto p-4 mt-4">
     {{ $slot }}
 </main>
+
+<script src="{{asset('/js/script.js')}}"></script>
 </body>
 </html>
