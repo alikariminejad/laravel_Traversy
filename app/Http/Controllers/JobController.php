5b7b6ad044868ba41a1fa96c7ab0a2a0ab45e9c9
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-01-19 23:30:46 +0330

    Model binding and single job listing

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use App\Models\Job;
 
 class JobController extends Controller
 {
@@ -12,12 +13,8 @@ class JobController extends Controller
      */
     public function index(): View
     {
-        $jobs = [
-            'PHP Developer',
-            'JavaScript Developer',
-            'Python Developer',
-        ];
-        return view('jobs.index', compact('jobs'));
+        $jobs = Job::all();
+        return view('jobs.index')->with('jobs', $jobs);
     }
 
     /**
@@ -39,9 +36,9 @@ class JobController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id): string
+    public function show(Job $job): string
     {
-        return "Show";
+        return view('jobs.show')->with('job', $job);
     }
 
     /**
