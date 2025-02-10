516299d346a38a970a4bdc2e79a50ba1ee2624c7
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-02-10 22:50:02 +0330

    Authentication and creating users

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\Support\ValidatedData;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 
@@ -14,5 +17,23 @@ class RegisterController extends Controller
     public function register(): View
     {
         return view('auth.register');
+    }
+
+    // @desc store User in database
+    // @route POST /register
+    public function store(Request $request): RedirectResponse
+    {
+        $ValidatedData = request()->validate([
+            'name' => 'required|string|max:100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // Hash password
+        $ValidatedData['password'] = Hash::make($ValidatedData['password']);
+
+        // Create User
+        $user = User::create($ValidatedData);
+        return redirect()->route('login')->with('success', 'You are registered and can log in!');
     }
 }
