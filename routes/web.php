939fd6ed9cf72773780276a95b9f4635fb132d85
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-02-11 22:08:26 +0330

    first middleware created

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Middleware\LogRequest;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
