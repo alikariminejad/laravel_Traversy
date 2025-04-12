18451836b5e44f34f1e2d50a5d3521e1914ecf9f
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-04-12 21:43:46 +0330

    Removing Bookmarks

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -21,17 +21,32 @@ class BookmarkController extends Controller
 
 
     // @desc    Create new bookmarked job
-    // @route   POST /bookmarks
+    // @route   POST /bookmarks/{job}
     public function store(Job $job): RedirectResponse{
         $user = Auth::user();
 
         // Check if the job is already bookmarked
         if($user->bookmarkedJobs()->where('job_id', $job->id)->exists()){
-            return back()->with('status', 'Job is already bookmarked');
+            return back()->with('error', 'Job is already bookmarked');
         }
 
         // Create new bookmark
         $job->bookmarkedJobs()->attach($job->id);
         return back()->with('success', 'Job bookmarked successfully');
+    }
+
+    // @desc    Delete bookmarked job
+    // @route   Delete /bookmarks/{job}
+    public function destroy(Job $job): RedirectResponse{
+        $user = Auth::user();
+
+        // Check if the job is not bookmarked
+        if(!$user->bookmarkedJobs()->where('job_id', $job->id)->exists()){
+            return back()->with('error', 'Job is not bookmarked');
+        }
+
+        // Remove bookmark
+        $job->bookmarkedJobs()->detach($job->id);
+        return back()->with('success', 'bookmark removed successfully');
     }
 }
